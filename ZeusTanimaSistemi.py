84cb34c40bee24d00586a5a8ff421b278bb7ef99
PolatBey01 <126326835+PolatBey01@users.noreply.github.com>
2023-05-24 12:27:47 +0300

    Update ZeusTanimaSistemi.py
    ZEUS BOT ASTRA İLE BİRLEŞEREK AYNI SİSTEMİ KULLANMAYA BAŞLADILAR

--- ZeusTanimaSistemi.py
+++ ZeusTanimaSistemi.py
@@ -1,26 +1,44 @@
 import time
-print("HOŞGELDİNİZ BEN ZEUS SİZİ TANIMAK İSTERİM")
+
+print("""
+[P]==========ZEUS=========[-][o][x]
+|     ZEUS Kayıt Programına       |
+|          Hoşgeldiniz!           |
+|           Sürüm 0.8             |
+|    Devam Etmek için Lütfen      |
+|          Bekleyiniz             |
+|             ...                 |
+|=================================|
+""")
 time.sleep(1)
 isim = input("Resmi Doğrulama İçin İsim,Soyisim Öğrenebilirmiyim:")
-yaş = int(input("Yaş (gerçek):")) 
-boy = float(input("Boy (m):"))
-kilo = int(input("Kilo (kg):"))
-cinsiyet = input("cinsiyet(E/K):")
+yaş = (input("Yaş (gerçek):")) 
+boy = (input("Boy (m):"))
+kilo = (input("Kilo (kg):"))
+cinsiyet = input("cinsiyet(Erkek/Kız):")
 
 
 print("""
 _____________________________________________
 """)
 print(isim)
-print(yaş)
-print(boy)
-print(kilo) 
-if cinsiyet == "e" :
-    print("erkek")
-elif cinsiyet == "k" :
-    print("kız")
+print(int (yaş))
+print(float(boy))
+print(int(kilo)) 
+print(cinsiyet)
 
 print("_____________________________________________")
- 
+time.sleep(1)
+doğruluk = input("BİLGİLER DOĞRUMU(DOĞRU İSE (e)YANLIŞ İSE (q)): ")
 
- 
+while True:
+  if doğruluk == "q":
+    break
+  
+  elif doğruluk == "e":
+    print("Yukaridaki Bilgileri Ekran Görüntüsü Alarak Bu Bağlantidaki Whatsapp Hesabimiza Göndererek Kayit Olabilirsiniz")
+    print("Bağlanti;")
+    print("https://wa.me/13854943023")
+    break
+  else :
+    break
